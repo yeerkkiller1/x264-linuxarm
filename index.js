09b59e074c43572e75de4f4e58cae9ceebd4da79
yeerkkiller1 <yeerkkiller1@gmail.com>
2018-06-25 19:28:21 -0400

    I think this executable should work on arm.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var child_process = require("child_process");
-
-child_process.execFileSync("chmod", ["a+x", __dirname + "/x264"]);
+child_process.execFileSync("chmod", ["a+x", "/home/pi/temp/node_modules/x264-linuxarm/x264"]);
 
 module.exports = function(...args) {
     return __dirname + "/x264";
