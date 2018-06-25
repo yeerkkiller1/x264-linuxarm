10ad7ca16c88c3ddee70ae3fee7314e50175edf5
yeerkkiller1 <yeerkkiller1@gmail.com>
2018-06-25 16:44:55 -0400

    Make x264 executable.

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+var child_process = require("child_process");
+
+child_process.execFileSync("chmod", ["a+x", __dirname + "/x264"]);
+
 module.exports = function(...args) {
     return __dirname + "/x264";
 };
