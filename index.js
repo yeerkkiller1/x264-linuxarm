33c0b57f66564141d071c86e4bcc5f6cdb7e21a4
yeerkkiller1 <yeerkkiller1@gmail.com>
2018-06-25 21:38:05 -0400

    The list of programs the fail to preserve permissions is growing...

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-var fs = require("fs");
+var child_process = require("child_process");
 
 // We need to do this because npm and yarn aren't setting permissions properly.
-fs.chmodSync(getX264Path(), 777);
+//  Also, we need to exec, as fs.chmodSync sets the permissions on other? Not the current user? Wtf...
+
+child_process.execFileSync("chmod", ["a+x", getX264Path()]);
 
 function getX264Path() {
     return __dirname + "/x264";
