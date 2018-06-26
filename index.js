85a28252b748ae135f4bd0cfc7e4392e90a2c3c0
yeerkkiller1 <yeerkkiller1@gmail.com>
2018-06-25 20:15:00 -0400

    Executable permissions in git... aren't working?

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-var child_process = require("child_process");
-child_process.execFileSync("chmod", ["a+x", "/home/pi/temp/node_modules/x264-linuxarm/x264"]);
-
-module.exports = function(...args) {
+function getX264Path() {
     return __dirname + "/x264";
-};
+}
+
+module.exports = getX264Path;
